519d6baf8f5aaab5f22b551da89038b9769ccf4e
harrygustavo <miranda.harry@gmail.com>
2023-10-24 23:22:36 -0500

    added cors

--- vetscreening/settings.py
+++ vetscreening/settings.py
@@ -98,10 +98,14 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://18.224.7.77",
+    "http://127.0.0.1:5173"
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:5173",  #
+    "http://localhost:5173",
+    "http://18.224.7.77",
+    "http://127.0.0.1:5173"  #
 ]
 
 
