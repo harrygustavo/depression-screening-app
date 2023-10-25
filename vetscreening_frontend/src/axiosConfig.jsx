dd52660a8554ccd4762298929d9dee688e4bd6b8
harrygustavo <miranda.harry@gmail.com>
2023-10-24 23:03:53 -0500

    axios

--- vetscreening_frontend/src/axiosConfig.jsx
+++ vetscreening_frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', // Replace with your Django backend base URL
+    baseURL: 'http://18.224.7.77/api/', // Replace with your Django backend base URL
 });
 
 export const setAuthToken = token => {
