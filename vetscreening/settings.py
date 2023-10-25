c9da6edbf01c8344c35fceb5d59fa7f7f830b179
harrygustavo <miranda.harry@gmail.com>
2023-10-25 00:02:22 -0500

    nginx

--- vetscreening/settings.py
+++ vetscreening/settings.py
@@ -100,13 +100,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://18.224.7.77",
     "http://127.0.0.1:5173",
-    "http://depression-screening-app-nginx-1"
+    "http://nginx"
 ]
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
     "http://18.224.7.77",
-    "http://127.0.0.1:5173"  #
+    "http://127.0.0.1:5173",
+    "http://nginx" #
 ]
 
 
