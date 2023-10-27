d67218d1e7465ac470de63e93a3694adb4279253
harrygustavo <miranda.harry@gmail.com>
2023-10-27 18:38:06 -0500

    ?

--- run-compose-dev.sh
+++ run-compose-dev.sh
@@ -9,7 +9,7 @@ export DEBUG=True
 export POSTGRES_DB=vetscreening_db
 export POSTGRES_USER=harrymiranda
 export POSTGRES_PASSWORD=password
-export  GOOGLE_API_KEY=$1
+export GOOGLE_API_KEY=$1
 docker-compose -f docker-compose.dev.yml up -d --build
 
 # make sure the postgres container is ready, then run migrations
