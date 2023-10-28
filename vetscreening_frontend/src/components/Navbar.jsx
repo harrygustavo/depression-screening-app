82a02e1b97780a97f544701ba3970fe604e1c3c9
harrygustavo <miranda.harry@gmail.com>
2023-10-28 02:23:04 -0500

    fin

--- vetscreening_frontend/src/components/Navbar.jsx
+++ vetscreening_frontend/src/components/Navbar.jsx
@@ -1,44 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ currentPage }) => {
     const [currentTime, setCurrentTime] = useState('');
     const [weather, setWeather] = useState(null);
 
     useEffect(() => {
+        // Fetching time
         axios.get('http://worldtimeapi.org/api/ip')
             .then(response => {
                 const date = new Date(response.data.datetime);
                 setCurrentTime(date.toLocaleString());
             })
             .catch(error => {
-                console.error('Error fetching time: ', error);
+                console.error('Error fetching time:', error);
             });
-    }, []);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-
-            axios.get(`https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`)
-                .then(response => {
-                    const location = response.data[0];
-
-                    return axios.get(`https://www.metaweather.com/api/location/${location.woeid}/`);
-                })
-                .then(response => {
-                    const todayWeather = response.data.consolidated_weather[0];
-                    setWeather({
-                        temp: todayWeather.the_temp,
-                        state: todayWeather.weather_state_name
-                    });
-                })
-                .catch(error => {
-                    console.error('Error fetching weather data:', error);
-                });
-        });
+        // Fetching weather
+        axios.get('https://wttr.in/?format=%t+%C')
+            .then(response => {
+                setWeather(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching weather:', error);
+            });
     }, []);
 
     return (
@@ -60,13 +46,9 @@ const Navbar = ({ currentPage }) => {
                 padding: 0
             }}>
                 <li>Today's date and time: {currentTime}</li>
-
-                {weather && (
-                    <li>Weather: {weather.temp.toFixed(2)}°C, {weather.state}</li>
-                )}
+                {weather && <li>Weather: {weather}</li>}
 
                 {/* Your existing navbar items here */}
-
             </ul>
         </nav>
     );
