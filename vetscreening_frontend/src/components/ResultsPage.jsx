47bdebb6e94dd0b9c380656c27c65e3243ecf517
harrygustavo <miranda.harry@gmail.com>
2023-10-28 01:23:41 -0500

    color

--- vetscreening_frontend/src/components/ResultsPage.jsx
+++ vetscreening_frontend/src/components/ResultsPage.jsx
@@ -38,7 +38,7 @@ const ResultsPage = () => {
                     <h4 style={{ marginBottom: '10px' }}>Here you can find Veterans Affairs Clinics and VA Resources close to you:</h4>
                     <button onClick={() => window.open("https://www.veteranscrisisline.net/find-resources/local-resources/", "_blank")} style={{
                         background: '#B22234',
-                        color: '#B22234',
+                        color: '#002366',
                         padding: '10px 20px',
                         borderRadius: '5px',
                         border: 'none',
