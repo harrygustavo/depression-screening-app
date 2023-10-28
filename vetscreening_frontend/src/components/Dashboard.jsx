cc6c56591b612692266f1df57f284c4ae7dff866
harrygustavo <miranda.harry@gmail.com>
2023-10-28 01:56:46 -0500

    weather

--- vetscreening_frontend/src/components/Dashboard.jsx
+++ vetscreening_frontend/src/components/Dashboard.jsx
@@ -3,63 +3,39 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 
-const base_url = import.meta.env.VITE_BASE_URL
+const base_url = import.meta.env.VITE_BASE_URL;
+const weather_api_key = 'YOUR_OPENWEATHERMAP_API_KEY'; // Add your OpenWeatherMap API key here
 
 const Dashboard = () => {
-    const [phq9History, setPHQ9History] = useState([]);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    // ... (other states here)
+    const [weather, setWeather] = useState(null);
+
+    // ... (other useEffects here)
 
     useEffect(() => {
-        // Check user authentication when the component mounts
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsAuthenticated(true);
-            // Fetch the user's PHQ-9 history when authenticated
-            fetchPHQ9History(token);
-        } else {
-            setIsAuthenticated(false);
-        }
+        navigator.geolocation.getCurrentPosition(async position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            try {
+                const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=imperial`);
+                setWeather(weatherResponse.data);
+            } catch (error) {
+                console.error('Error fetching weather data:', error);
+            }
+        });
     }, []);
 
-    const fetchPHQ9History = async (token) => {
-        try {
-            const response = await axios.get(`http://${base_url}/api/users/phq9/list/`, {
-                headers: {
-                    'Authorization': `Token ${token}`,
-                },
-            });
-
-            if (response.status === 200) {
-                setPHQ9History(response.data);
-            }
-        } catch (error) {
-            console.error('Error fetching PHQ-9 history:', error);
+    const renderWeather = () => {
+        if (weather) {
+            const temperature = weather.main.temp;
+            const description = weather.weather[0].description;
+            return (
+                <div style={{ marginTop: '20px' }}>
+                    <h4>Current Weather: {temperature}°F, {description}</h4>
+                </div>
+            );
         }
-    };
-
-    const getScreeningResult = (totalScore) => {
-        if (totalScore < 10) {
-            return 'Negative';
-        } else {
-            return 'Positive';
-        }
-    };
-
-    const formatDate = (timestamp) => {
-        const date = new Date(timestamp);
-        const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-        };
-        return format(date, "MMMM d, yyyy 'at' h:mmaaa", options);
-    };
-
-    const getScoreStyle = (totalScore) => {
-        return totalScore < 10 ? {} : { color: 'red' };
     };
 
     return (
@@ -74,64 +50,11 @@ const Dashboard = () => {
             justifyContent: 'center',
             padding: '20px'
         }}>
-            <h1>Welcome to Your Dashboard</h1>
-            <h4 style={{
-                textAlign: 'center',
-                maxWidth: '600px',
-                margin: '20px auto',
-            }}>
-               See all of your screening results over time 
-            </h4>
-            
-            {isAuthenticated && phq9History.length > 0 ? (
-                <div>
-                    <h2>Your PHQ-9 History</h2>
-                    <ul style={{
-                        listStyleType: 'none',
-                        padding: 0,
-                    }}>
-                        {phq9History.map((result) => (
-                            <li key={result.id} style={{
-                                padding: '15px',
-                                borderRadius: '5px',
-                                marginBottom: '10px',
-                            }}>
-                                On {formatDate(result.timestamp)}, your score was{' '}
-                                <span style={getScoreStyle(result.total_score)}>
-                                    {getScreeningResult(result.total_score)}
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ) : (
-                <p>Once you take a depression screening, your history will be displayed here.</p>
-            )}
+            {/* ... (other parts of your JSX here) */}
 
-            <div style={{ marginTop: '20px' }}>
-                <Link to="/logout" style={{
-                    backgroundColor: '#B22234',
-                    color: '#fff',
-                    padding: '10px 20px',
-                    borderRadius: '5px',
-                    textDecoration: 'none',
-                    fontWeight: 'bold',
-                    marginRight: '15px',
-                }}>
-                    Logout
-                </Link>
+            {renderWeather()}
 
-                <Link to="/questionnaire" style={{
-                    backgroundColor: '#B22234',
-                    color: '#fff',
-                    padding: '10px 20px',
-                    borderRadius: '5px',
-                    textDecoration: 'none',
-                    fontWeight: 'bold',
-                }}>
-                    Go to Questionnaire
-                </Link>
-            </div>
+            {/* ... (other parts of your JSX here) */}
         </div>
     );
 };
